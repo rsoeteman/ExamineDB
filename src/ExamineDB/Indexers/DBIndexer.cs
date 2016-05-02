9f922ef8f3014af5c93bce2b5468fcf0aac023ae
Richard Soeteman <richard@soetemansoftware.nl>
2016-05-02 11:46:47 +0200

    Added source project

---
+++ src/ExamineDB/Indexers/DBIndexer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Examine.Providers;
+
+namespace ExamineDB.Indexers
+{
+    public class DBIndexer : BaseIndexProvider
+    {
+
+        public override void ReIndexNode(XElement node, string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteFromIndex(string nodeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void IndexAll(string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RebuildIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IndexExists()
+        {
+            throw new NotImplementedException();
+        }
+
+      
+    }
+}
---
+++ src/ExamineDB/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExamineDB")]
+[assembly: AssemblyDescription("Simple tool that makes it possible to index database data using Examine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Soeteman software")]
+[assembly: AssemblyProduct("ExamineDB")]
+[assembly: AssemblyCopyright("Copyright ©  Soeteman software  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2cbb61c5-c985-4e85-a101-994e8e0d094d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
